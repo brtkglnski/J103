579b5b1270b696ad4017f7ce9dd7b7113c7bb99b
brtkglnski <glinskib2008@gmail.com>
2025-12-15 10:00:10 +0100

    matchup page styling

--- src/routes/coopRouter.js
+++ src/routes/coopRouter.js
@@ -14,7 +14,7 @@ router.post('/match', coopController.match);
 router.get('/profile', coopController.viewProfile);
 router.post('/profile/edit',uploadAvatar.single('avatar'), coopController.updateProfile);
 router.get('/profile/:slug', coopController.viewProfile);
-router.get('/profile/:slug/incoming', coopController.viewIncomingRequests);
+router.get('/profile/:slug/incoming', coopController.viewIncomingRequests); //make it so others cant see another users requests
 router.post('/profile/:slug/incoming', coopController.manageIncomingRequest);
 router.get('/profile/:slug/outgoing', coopController.viewOutgoingRequests);
 router.post('/profile/:slug/outgoing', coopController.manageOutgoingRequest);
